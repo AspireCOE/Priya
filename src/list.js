491ec96957f0ffc9641f594a37b536ca9e3db9f7
Priya732003 <125880904+Priya732003@users.noreply.github.com>
2024-04-01 10:27:07 +0530

    Add files via upload

---
+++ src/list.js
@@ -0,0 +1,10 @@
+var fruits = ["apple", "orange", "grapes"];
+fruits.push("papaya");
+fruits.pop();
+fruits.forEach(function (fruit) {
+    console.log(fruit);
+});
+var mixedList = [1, "priya", true];
+mixedList.forEach(function (element) {
+    console.log(element);
+});
---
+++ src/list.ts
@@ -0,0 +1,11 @@
+const fruits:string[]=["apple","orange","grapes"];
+fruits.push("papaya");
+fruits.pop();
+fruits.forEach(fruit=>{
+    console.log(fruit);
+})
+
+const mixedList=[1,"priya",true];
+mixedList.forEach(element=>{
+    console.log(element);
+})
